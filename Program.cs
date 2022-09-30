9383169bb87badc7f64d2a2e32208d546b660077
salallegra <salvatore.allegra@smoothstack.com>
2022-09-30 11:40:12 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // e.g. https://yourorg.crm.dynamics.com
+        string url = "*****************";
+        // e.g. you@yourorg.onmicrosoft.com
+        string userName = "************";
+        // e.g. y0urp455w0rd 
+        string password = "***************";
+
+        string conn = $@"
+        Url = {url};
+        AuthType = OAuth;
+        UserName = {userName};
+        Password = {password};
+        AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
+        RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+        LoginPrompt=Auto;
+        RequireNewInstance = True";
+
+        using (var svc = new CrmServiceClient(conn))
+        {
+
+            WhoAmIRequest request = new WhoAmIRequest();
+
+            WhoAmIResponse response = (WhoAmIResponse)svc.Execute(request);
+
+            Console.WriteLine("Your UserId is {0}", response.UserId);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+
+}
+
