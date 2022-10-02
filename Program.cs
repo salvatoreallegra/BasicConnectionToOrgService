d8055d46d93ca830655aeaba23b7331005c3cd2d
salallegra <salvatore.allegra@smoothstack.com>
2022-10-02 01:24:09 -0700

    Send a fetchexpression to the org service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
 using System;
 
@@ -8,11 +11,18 @@ public class Program
     static void Main(string[] args)
     {
         // e.g. https://yourorg.crm.dynamics.com
-        string url = "*****************";
+        //string url = "*****************";
         // e.g. you@yourorg.onmicrosoft.com
-        string userName = "************";
+        //string userName = "************";
         // e.g. y0urp455w0rd 
-        string password = "***************";
+        //string password = "***************";
+
+        // e.g. https://yourorg.crm.dynamics.com
+        string url = "https://org7ba21b0e.crm.dynamics.com/";
+        // e.g. you@yourorg.onmicrosoft.com
+        string userName = "SalvatoreAllegra@NoCo7775.onmicrosoft.com";
+        // e.g. y0urp455w0rd 
+        string password = "JediKnight1@7775";
 
         string conn = $@"
         Url = {url};
@@ -33,9 +43,32 @@ public class Program
 
             Console.WriteLine("Your UserId is {0}", response.UserId);
 
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
+            
         }
+
+        string fetch2 = @"  
+           <fetch mapping='logical'>  
+             <entity name='account'>   
+                <attribute name='accountid'/>   
+                <attribute name='name'/>   
+                <link-entity name='systemuser' to='owninguser'>   
+                   <filter type='and'>   
+                      <condition attribute='lastname' operator='ne' value='Cannon' />   
+                   </filter>   
+                </link-entity>   
+             </entity>   
+           </fetch> "
+                ;
+        using (var svc = new CrmServiceClient(conn))
+        {
+            EntityCollection result = svc.RetrieveMultiple(new FetchExpression(fetch2));
+            
+            foreach (var c in result.Entities)
+            {
+                System.Console.WriteLine(c.Attributes["name"]);
+            }
+        }
+        Console.ReadLine();
     }
 
 }
